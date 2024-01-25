57a7251f15643cd11a02473aae69ceefd708babb
Michelle <jiarongwu0130m@gmail.com>
2024-01-25 09:36:02 +0800

    Add Project

---
+++ SalesForGem/WebApplication1/WebApplication1/Models/Entity/Sell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models.Entity;
+
+public partial class Sell
+{
+    public int Id { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string DocumentNumber { get; set; } = null!;
+
+    public int CustomerId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public int Quantity { get; set; }
+
+    public string? Remarks { get; set; }
+
+    public virtual Customer Customer { get; set; } = null!;
+
+    public virtual Product Product { get; set; } = null!;
+}
